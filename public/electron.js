8d869cbca95888675543d7e3c61cdee06cffcb49
walbermr <wmr@cin.ufpe.br>
2023-10-10 00:47:08 -0300

    Readicionando devtools e fix em entity e migration

--- public/electron.js
+++ public/electron.js
@@ -16,7 +16,7 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-      devTools: false,
+      devTools: true,
     },
   });
   mainWindow.loadURL(
